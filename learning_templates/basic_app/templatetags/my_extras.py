0118cadc2a3607d7ed6ff7b425b3f71ab837c2d3
Joan Prat Rigol <pratjunior@gmail.com>
2017-12-16 14:47:27 +0100

    'first'

---
+++ learning_templates/basic_app/templatetags/my_extras.py
@@ -0,0 +1,21 @@
+from django import template
+
+register = template.Library()
+
+
+# def cut(value, arg):
+#     """
+#     this cuts out all values of "arg" from the string
+#     """
+#     return value.replace(arg, '')
+#
+#
+# register.filter('cut', cut)
+
+
+@register.filter(name='cut')
+def cut(value, arg):
+    """
+    this cuts out all values of "arg" from the string
+    """
+    return value.replace(arg, '')
